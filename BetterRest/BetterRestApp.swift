39b35eec0b83042449717b071802fc272b0eac04
Evdokimov Denis <analitik9@gmail.com>
2024-05-27 16:08:24 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Denis Evdokimov on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
